1e4267c396fff9623bd58ad4a44e3227beb1277c
lisandrofernando <lisandrusfernandus@gmail.com>
2022-05-30 18:23:37 -0500

    Adding some tests

--- Pages/ecomercePage.ts
+++ Pages/ecomercePage.ts
@@ -1,9 +1,13 @@
 import { Selector } from "testcafe";
 
 class ecomerce {
-  logo: Selector = Selector("#header_logo");
-  woman: Selector = Selector('[title="Women"]');
-  tshirt: Selector = Selector('[title="T-shirts"]');
+  loginLogo: Selector = Selector(".login_logo");
+  logo: Selector = Selector("span.title").withAttribute('Products');
+  user: Selector = Selector("#user-name");
+  password: Selector = Selector("#password");
+  loginButton: Selector = Selector("#login-button");
+  firstProduct: Selector = Selector('#item_4_title_link');
+  backToProduct: Selector = Selector('#back-to-products');
 }
 
 export default new ecomerce();
--- Tests/E2Etest.ts
+++ Tests/E2Etest.ts
@@ -1,12 +1,36 @@
 import { Selector, t } from "testcafe";
 import ecomerce from "../Pages/ecomercePage";
 
-const url = "http://automationpractice.com/index.php";
+const url = "https://www.saucedemo.com";
 
 fixture("Ecomerce Store").page(url);
 
-test("User Lands in Ecomerce Store", async (t) => {
-  await t.expect(ecomerce.logo.innerText).contains("");
-  await t.hover(ecomerce.woman);
-  await t.click(ecomerce.tshirt);
+test("User Lands in Sauce Demo", async (t) => {
+  await t.expect(ecomerce.loginLogo.innerText).contains("");
+  await t.typeText(ecomerce.user,'standard_user');
+  await t.typeText(ecomerce.password,'secret_sauce');
+});
+
+test("User Lands in Sauce Demo and checks the landing page", async (t) => {
+  await t.expect(ecomerce.loginLogo.innerText).contains("");
+  await t.typeText(ecomerce.user,'standard_user');
+  await t.typeText(ecomerce.password,'secret_sauce');
+  await t.click(ecomerce.loginButton);
+});
+
+test("User Lands in Sauce Demo and checks the landing page and the first link", async (t) => {
+  await t.expect(ecomerce.loginLogo.innerText).contains("");
+  await t.typeText(ecomerce.user,'standard_user');
+  await t.typeText(ecomerce.password,'secret_sauce');
+  await t.click(ecomerce.loginButton);
+  await t.click(ecomerce.firstProduct);
+});
+
+test("User Lands in Sauce Demo and checks the landing page and the first link & Back to products", async (t) => {
+  await t.expect(ecomerce.loginLogo.innerText).contains("");
+  await t.typeText(ecomerce.user,'standard_user');
+  await t.typeText(ecomerce.password,'secret_sauce');
+  await t.click(ecomerce.loginButton);
+  await t.click(ecomerce.firstProduct);
+  await t.click(ecomerce.backToProduct);
 });
