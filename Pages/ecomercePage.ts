5edbee3044d64c25fab67287cfa71a8e2d86bb3e
lisandrofernando <lisandrusfernandus@gmail.com>
2022-05-30 18:46:43 -0500

    Added Tests

--- Pages/ecomercePage.ts
+++ Pages/ecomercePage.ts
@@ -8,6 +8,9 @@ class ecomerce {
   loginButton: Selector = Selector("#login-button");
   firstProduct: Selector = Selector('#item_4_title_link');
   backToProduct: Selector = Selector('#back-to-products');
+  addToCart: Selector = Selector('#add-to-cart-sauce-labs-backpack');
+  shoppingCartContainer: Selector = Selector('#shopping_cart_container');
+  removeFromCart: Selector = Selector('#remove-sauce-labs-backpack');
 }
 
 export default new ecomerce();
--- Tests/E2Etest.ts
+++ Tests/E2Etest.ts
@@ -34,3 +34,26 @@ test("User Lands in Sauce Demo and checks the landing page and the first link & 
   await t.click(ecomerce.firstProduct);
   await t.click(ecomerce.backToProduct);
 });
+
+test("User Lands in Sauce Demo and checks the landing page and the first link & Back to products", async (t) => {
+  await t.expect(ecomerce.loginLogo.innerText).contains("");
+  await t.typeText(ecomerce.user,'standard_user');
+  await t.typeText(ecomerce.password,'secret_sauce');
+  await t.click(ecomerce.loginButton);
+  await t.click(ecomerce.firstProduct);
+  await t.click(ecomerce.addToCart);
+  await t.click(ecomerce.shoppingCartContainer);
+  await t.expect(ecomerce.firstProduct.innerText).contains('Sauce Labs Backpack');
+});
+
+test("User Lands in Sauce Demo and checks the landing page and the first link & Back to products", async (t) => {
+  await t.expect(ecomerce.loginLogo.innerText).contains("");
+  await t.typeText(ecomerce.user,'standard_user');
+  await t.typeText(ecomerce.password,'secret_sauce');
+  await t.click(ecomerce.loginButton);
+  await t.click(ecomerce.firstProduct);
+  await t.click(ecomerce.addToCart);
+  await t.click(ecomerce.shoppingCartContainer);
+  await t.expect(ecomerce.firstProduct.innerText).contains('Sauce Labs Backpack');
+  await t.click(ecomerce.removeFromCart);
+});
