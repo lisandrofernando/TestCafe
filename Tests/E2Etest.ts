1bf3fb1cf7c6894783d54e9085c05587c9ed21e2
lisandrofernando <lisandrusfernandus@gmail.com>
2024-05-27 15:22:05 -0600

    Work In Progress

--- Tests/E2Etest.ts
+++ Tests/E2Etest.ts
@@ -1,6 +1,8 @@
 import { Selector, t } from "testcafe";
 import ecomerce from "../Pages/ecomercePage";
 
+// Fixture is the name of the suite
+
 const url = "https://www.saucedemo.com";
 
 fixture("Ecomerce Store").page(url);
@@ -16,6 +18,7 @@ test("User Lands in Sauce Demo and checks the landing page", async (t) => {
   await t.typeText(ecomerce.user,'standard_user');
   await t.typeText(ecomerce.password,'secret_sauce');
   await t.click(ecomerce.loginButton);
+  //await t.expect(ecomerce.titleHeader.value).contains('Products');
 });
 
 test("User Lands in Sauce Demo and checks the landing page and the first link", async (t) => {
